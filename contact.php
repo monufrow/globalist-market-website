f0f914d2f3588448366d0144061f5e50d9a100fb
Mitch Onufrow <monufrow@trinity.edu>
2026-07-21 08:45:11 -0500

    contact form and styles changes for all pages

---
+++ contact.php
@@ -0,0 +1,17 @@
+<?php
+
+// 1. Only accept POST requests
+
+// 2. Read the form values
+
+// 3. Validate required fields
+
+// 4. Stop spam
+
+// 5. Create email
+
+// 6. Send email with PHPMailer
+
+// 7. Redirect user
+
+?>
