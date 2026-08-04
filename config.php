61d52de0f3758bab3f31f00c25de04138d985f7a
Mitch Onufrow <monufrow@trinity.edu>
2026-08-04 08:18:01 -0500

    push before refinement

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 define('SMTP_HOST', 'mail.globalistmarket.com');
 define('SMTP_USER', 'info@globalistmarket.com');
-define('SMTP_PASS', 'n3p59L6fwgi6K8D');
+define('SMTP_PASS', '');
 define('SMTP_PORT', 465);
 define('SMTP_ENCRYPTION', 'ssl'); // Use 'ssl' for port 465 or 'tls' for port 587
 
