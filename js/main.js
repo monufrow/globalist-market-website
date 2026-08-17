ebf493601b755e936b18a9126b0e75839aec8d6d
Mitch Onufrow <monufrow@trinity.edu>
2026-08-17 11:06:33 -0500

    sticky navbar and back to top button consolidation

--- js/main.js
+++ js/main.js
@@ -1,39 +1,21 @@
 (function ($) {
     "use strict";
-
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
     
-    
-    // Initiate the wowjs
+    // // Initialize WOW.js animations
     new WOW().init();
 
 
-    // Sticky Navbar
     $(window).scroll(function () {
+        // Sticky Navbar
         if ($(this).scrollTop() > 300) {
-            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px'); //sticky navbar
+            $('.back-to-top').fadeIn('slow'); // back to top button
         } else {
-            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px'); // sticky navbar
+            $('.back-to-top').fadeOut('slow'); // back to top button
         }
     });
     
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
